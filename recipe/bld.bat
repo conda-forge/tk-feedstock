defcbefa9557974d9263cc4c5623123c081912d7
Jonathan Helmus <jjhelmus@gmail.com>
2025-12-19 12:10:27 -0600

    update bld.bat for new version

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -37,10 +37,10 @@ nmake -f makefile.vc INSTALLDIR=%LIBRARY_PREFIX% MACHINE=%MACHINE% TCLDIR=..\..\
 if %ERRORLEVEL% GTR 0 exit 1
 
 :: Make sure that `wish` can be called without the version info.
-copy %LIBRARY_PREFIX%\bin\wish86t.exe %LIBRARY_PREFIX%\bin\wish.exe
-copy %LIBRARY_PREFIX%\bin\tclsh86t.exe %LIBRARY_PREFIX%\bin\tclsh.exe
+copy %LIBRARY_PREFIX%\bin\wish90t.exe %LIBRARY_PREFIX%\bin\wish.exe
+copy %LIBRARY_PREFIX%\bin\tclsh90t.exe %LIBRARY_PREFIX%\bin\tclsh.exe
 
 :: No `t` version of wish86.exe
-copy %LIBRARY_PREFIX%\bin\wish86t.exe %LIBRARY_PREFIX%\bin\wish86.exe
-copy %LIBRARY_PREFIX%\bin\tclsh86t.exe %LIBRARY_PREFIX%\bin\tclsh86.exe
+copy %LIBRARY_PREFIX%\bin\wish90t.exe %LIBRARY_PREFIX%\bin\wish90.exe
+copy %LIBRARY_PREFIX%\bin\tclsh90t.exe %LIBRARY_PREFIX%\bin\tclsh90.exe
 popd
